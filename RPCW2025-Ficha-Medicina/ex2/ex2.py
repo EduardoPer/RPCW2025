e77d0a3f4806daeca353505099da9ae51ed85260
EduardoPer <43eduardo43@gmail.com>
2025-04-06 21:56:36 +0100

    feito até ao 2.10

--- RPCW2025-Ficha-Medicina/ex2/ex2.py
+++ RPCW2025-Ficha-Medicina/ex2/ex2.py
@@ -1,6 +1,4 @@
 import json
-import pandas
-import re
 
 
 ds_txt = ""
@@ -8,6 +6,9 @@ ds_dict = {}
 dd_txt = ""
 dd_dict = {}
 symptoms_to_add = []
+dt_txt = ""
+dt_dict = {}
+treatments_to_add = []
 
 with open("Disease_Syntoms.csv", "r", encoding="utf-8") as f:
     ds_txt = f.read()
@@ -16,11 +17,11 @@ ds_txt = ds_txt.split("\n")[1:]
 
 for l in ds_txt:
     l = l.split(",")
-    l[0] = l[0].strip().replace(" ", "_").replace("(", "_").replace(")", "_")
+    l[0] = l[0].strip().replace(" ", "_").replace("(", "_").replace(")", "_").strip("_").replace("__", "_").lower()
     if l[0] not in ds_dict.keys() and l[0] != "":
         ds_dict[l[0]] = []
     for s in l[1:]:
-        s = s.strip().replace(" ", "_").replace("(", "_").replace(")", "_")
+        s = s.strip().replace(" ", "_").replace("(", "_").replace(")", "_").strip("_").replace("__", "_").lower()
         if s not in ds_dict[l[0]] and s != "":
             ds_dict[l[0]].append(s)
             s = f":{s} a :Symptom ."
@@ -34,10 +35,50 @@ dd_txt = dd_txt.split("\n")[1:]
 
 for l in dd_txt:
     l = l.split(",", 1)
+    l[0] = l[0].strip().replace(" ", "_").replace("(", "_").replace(")", "_").strip("_").replace("__", "_").lower()
     if len(l) > 1:
         if l[0] not in dd_dict.keys():
             dd_dict[l[0]] = []
         dd_dict[l[0]].append(l[1].strip().replace('"', ''))
+        
+with open("Disease_Treatment.csv", "r", encoding="utf-8") as f:
+    dt_txt = f.read()
+
+dt_txt = dt_txt.split("\n")[1:]
+
+for l in dt_txt:
+    l = l.split(",", 4)
+    l[0] = l[0].strip().replace(" ", "_").replace("(", "_").replace(")", "_").strip("_").replace("__", "_").lower()
+    if l[0] not in dt_dict.keys() and l[0] != "":
+        dt_dict[l[0]] = []
+    for s in l[1:]:
+        s = s.strip().replace(" ", "_").replace("(", "_").replace(")", "_").strip("_").replace("__", "_").lower()
+        if s not in dt_dict[l[0]] and s != "":
+            dt_dict[l[0]].append(s)
+            s = f":{s} a :Treatment ."
+            if s not in treatments_to_add:
+                treatments_to_add.append(s)
+
+doentes_json = []
+doentes_ont = ""
+with open("doentes.json", "r", encoding='utf-8') as f:
+    doentes_json = json.load(f)
+
+doente_id = 1
+for d in doentes_json:
+    doentes_ont += f':Patient{doente_id} a :Patient '
+    if "nome" in d.keys():
+        doentes_ont += f';\n\t:name "{d["nome"]}" '
+    if "sintomas" in d.keys():
+        sintomas_added = []
+        for sint in d["sintomas"]:
+            sint = sint.strip().replace(" ", "_").replace("(", "_").replace(")", "_").strip("_").replace("__", "_").lower()
+            if sint not in sintomas_added:
+                sintomas_added.append(sint)
+                doentes_ont += f';\n\t:exhibitsSymptom :{sint} '
+    doentes_ont += '.\n\n'
+    doente_id += 1
+
                 
 ont_txt = '''@prefix : <http://www.example.org/disease-ontology#> .
 @prefix owl: <http://www.w3.org/2002/07/owl#> .
@@ -80,6 +121,7 @@ ont_txt = '''@prefix : <http://www.example.org/disease-ontology#> .
                      rdfs:domain :Disease ;
                      rdfs:range xsd:string .
 
+#Disease instances
 '''
 
 for k,v in ds_dict.items():
@@ -101,11 +143,27 @@ for k,v in ds_dict.items():
                     ont_txt += f'''"{descs[i]}" '''
                 else:
                     ont_txt += f''', "{descs[i]}" '''
-    ont_txt += '.\n\n'
     
+    if dt_dict.get(k) != None:
+        treats = dt_dict[k]
+        if treats != []:
+            ont_txt += ';\n\t:hasTreatment '
+            for i in range(0, len(treats)):
+                if i == 0:
+                    ont_txt += f'''"{treats[i]}" '''
+                else:
+                    ont_txt += f''', "{treats[i]}" '''
+                    
+    ont_txt += '.\n\n'
+
+ont_txt += '#Symptoms instances\n'
 for sym in symptoms_to_add:
     ont_txt += sym + "\n"
-    
-with open("med_doencas.ttl", "w", encoding='utf-8') as f:
+ont_txt += '\n#Treatments instances\n'
+for treat in treatments_to_add:
+    ont_txt += treat + "\n"
+ont_txt += '\n#Patient instances\n' + doentes_ont
+
+with open("med_doentes.ttl", "w", encoding='utf-8') as f:
     f.write(ont_txt)
         
